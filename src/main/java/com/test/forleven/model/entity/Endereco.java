22ec79ff5ff2e358a568532c77be7786aa62b90a
Reynaldo Hendson <reynaldohendsondev@outlook.com>
2024-06-26 18:42:58 -0300

    feat entidade endereço

--- src/main/java/com/test/forleven/model/entity/Endereco.java
+++ src/main/java/com/test/forleven/model/entity/Endereco.java
@@ -1,5 +1,6 @@
 package com.test.forleven.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
@@ -11,10 +12,16 @@ import lombok.Setter;
 public class Endereco {
 
 	@Id
+	@JsonProperty("cep")
 	private String cep;
+
 	private String logradouro;
+
 	private String complemento;
+
 	private String bairro;
+
 	private String localidade;
+
 	private String uf;
 }
