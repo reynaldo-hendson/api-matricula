52dab4d3d761c19a19ff0504e4b281beb58ebac4
Reynaldo Hendson <reynaldohendsondev@outlook.com>
2024-06-26 18:26:21 -0300

    feat mapeamento de lista de alunos estudante mapper

--- src/main/java/com/test/forleven/api/mapper/EstudanteResumeResponseMapper.java
+++ src/main/java/com/test/forleven/api/mapper/EstudanteResumeResponseMapper.java
@@ -4,8 +4,13 @@ import com.test.forleven.api.response.EstudanteResumeResponse;
 import com.test.forleven.model.entity.Estudante;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EstudanteResumeResponseMapper {
     EstudanteResumeResponse toEstudanteResumeResponse(Estudante estudante);
+
+    List<EstudanteResumeResponse> toEstudanteResumeResponseList(List<Estudante> estudantes);
+
 
 }
