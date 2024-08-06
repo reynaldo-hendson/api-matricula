952a1eaafdc818c5867bfb884418de04fbc78601
Reynaldo Hendson <reynaldohendsondev@outlook.com>
2024-08-06 20:07:37 -0300

    cors

--- src/main/java/com/test/forleven/ForlevenApplication.java
+++ src/main/java/com/test/forleven/ForlevenApplication.java
@@ -1,15 +1,23 @@
 package com.test.forleven;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(servers ={@Server(url = "/", description = "Default Server URL")})
 public class ForlevenApplication {
 
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(ForlevenApplication.class, args);
 	}
 
 }
+
+
+
+
